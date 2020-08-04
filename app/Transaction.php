138d0738a74a65a51f7dd45c2c43e21c7c73fe6f
fauzigalih <fauzigalihas@gmail.com>
2020-08-04 23:18:09 +0700

    create model transaction

---
+++ app/Transaction.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    //table transaction on migration
+    protected $table = 'transaction';
+}
