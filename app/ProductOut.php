a4d9b0540e19d82699cf2ca90153647de6a19d6a
fauzigalih <fauzigalihas@gmail.com>
2020-08-04 23:17:59 +0700

    create model product out

---
+++ app/ProductOut.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductOut extends Model
+{
+    //table product_out on migration
+    protected $table = 'product_out';
+}
