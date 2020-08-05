443d70906200a12d656f2290e01055f57d0b3023
fauzigalih <fauzigalihas@gmail.com>
2020-08-05 22:57:56 +0700

    Create ProductInController.php

---
+++ app/Http/Controllers/ProductInController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ProductIn;
+use Illuminate\Http\Request;
+
+class ProductInController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ProductIn  $productIn
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProductIn $productIn)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ProductIn  $productIn
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProductIn $productIn)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ProductIn  $productIn
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ProductIn $productIn)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ProductIn  $productIn
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProductIn $productIn)
+    {
+        //
+    }
+}
