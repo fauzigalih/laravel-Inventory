924484f1027b9276e2ea972fe776f622e6505f46
fauzigalih <fauzigalihas@gmail.com>
2020-08-06 22:25:42 +0700

    route control

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Routing\Route as RoutingRoute;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,71 +13,19 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', 'PageController@index');
+Route::get('/signin', 'PageController@signin');
+Route::get('/signup', 'PageController@signup');
 
-Route::get('/', function () {
-    return view('pages.index');
-});
+Route::get('/product', 'ProductController@index');
+Route::get('/product/create', 'ProductController@create');
 
-Route::get('/email', function() {
-    return view('pages.email');
-});
+Route::get('/product-in', 'ProductInController@index');
 
-Route::get('/compose', function() {
-    return view('pages.compose');
-});
-
-Route::get('/calendar', function() {
-    return view('pages.calendar');
-});
-
-Route::get('/chat', function() {
-    return view('pages.chat');
-});
-
-Route::get('/charts', function() {
-    return view('pages.charts');
-});
-
-Route::get('/forms', function() {
+Route::get('forms', function () {
     return view('pages.forms');
 });
 
-Route::get('/ui', function() {
-    return view('pages.ui');
-});
-
-Route::get('/basic-table', function() {
-    return view('pages.basic-table');
-});
-
-Route::get('/data-table', function() {
+Route::get('table', function () {
     return view('pages.data-table');
-});
-
-Route::get('/google-maps', function() {
-    return view('pages.google-maps');
-});
-
-Route::get('/vector-maps', function() {
-    return view('pages.vector-maps');
-});
-
-Route::get('/blank', function() {
-    return view('pages.blank');
-});
-
-Route::get('/404', function() {
-    return view('pages.404');
-});
-
-Route::get('/500', function() {
-    return view('pages.500');
-});
-
-Route::get('/signin', function() {
-    return view('pages.signin');
-});
-
-Route::get('/signup', function() {
-    return view('pages.signup');
 });
