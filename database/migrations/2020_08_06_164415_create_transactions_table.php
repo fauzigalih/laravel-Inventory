4de18974477602f1ef4c31b3e19edf2f86563497
fauzigalih <fauzigalihas@gmail.com>
2020-08-06 22:27:04 +0700

    adjust new migration

---
+++ database/migrations/2020_08_06_164415_create_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('invoice', 50);
+            $table->integer('userId');
+            $table->string('codeTrx', 50);
+            $table->integer('stockFirst');
+            $table->integer('qtyTrx');
+            $table->integer('stockFinal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
