266d12cfaa0cd16a6e39c5a75be0d969ec01ad83
fauzigalih <fauzigalihas@gmail.com>
2020-08-07 22:59:45 +0700

    set fillable

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+        'invoice',
+        'name_product',
+        'type_product',
+        'unit',
+        'price',
+        'stock_first',
+        'stock_in',
+        'stock_out',
+        'stock_final',
+        'image_product',
+        'active'
+    ];
 }
