4f0eb4ec50ddbe09d473d379d719ef2ba97e39cb
fauzigalih <fauzigalihas@gmail.com>
2020-08-07 23:01:05 +0700

    move main

--- resources/views/components/form/main.blade.php
+++
@@ -1,4 +0,0 @@
-@php
-    Form::component('textGroup', 'components.form.text', ['name', 'value' => null, 'attributes' => []]);
-    Form::component('emailGroup', 'components.form.email', ['name', 'value' => null, 'attributes' => []]);
-@endphp
