5d8088c46cec272dd92e2e1dade0db01fe1db896
fauzigalih <fauzigalihas@gmail.com>
2020-08-08 22:47:48 +0700

    add col class

--- resources/views/components/form/email.blade.php
+++ resources/views/components/form/email.blade.php
@@ -1,4 +1,4 @@
-<div class="form-group">
+<div class="form-group col-md-4">
     {{ Form::label($name, null, ['class' => 'control-label']) }}
     {{ Form::email($name, $value, array_merge(['class' => 'form-control', 'placeholder' => 'name@domain.com'], $attributes)) }}
 </div>
