aa6e9491111867203841df01794919d0e5ea014f
fauzigalih <fauzigalihas@gmail.com>
2020-08-08 22:48:24 +0700

    add col class and add attribute id

--- resources/views/components/form/text.blade.php
+++ resources/views/components/form/text.blade.php
@@ -1,4 +1,12 @@
-<div class="form-group">
+<div class="form-group col-md-4">
     {{ Form::label($name, null, ['class' => 'control-label']) }}
-    {{ Form::text($name, $value, array_merge(['class' => 'form-control', 'placeholder' => ucwords(str_replace('_', ' ', $name))], $attributes)) }}
+    {{ Form::text($name, $value, 
+        array_merge(
+            [
+                'class' => 'form-control', 
+                'placeholder' => ucwords(str_replace('_', ' ', $name)),
+                'id' => $name,
+            ], 
+            $attributes,
+        )) }}
 </div>
