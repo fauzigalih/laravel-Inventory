85525922dcd4c97077bc2a31a94095d243e30705
fauzigalih <fauzigalihas@gmail.com>
2020-08-08 22:49:16 +0700

    adjust format and add format form

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -4,7 +4,8 @@
     Form::component('passwordGroup', 'components.form.password', ['name', 'value' => null, 'attributes' => []]);
     Form::component('numberGroup', 'components.form.number', ['name', 'value' => null, 'attributes' => []]);
     Form::component('fileGroup', 'components.form.file', ['name', 'attributes' => []]);
-    Form::component('selectGroup', 'components.form.select', ['name', 'attributes' => []]);
+    Form::component('selectGroup', 'components.form.select', ['name', 'value' => null, 'attributes' => []]);
+    Form::component('selectTypeGroup', 'components.form.selectType', ['name', 'value' => null, 'attributes' => []]);
 
     HTML::component('linkIcon', 'components.html.link', ['url', 'title', 'icon' => null, 'attributes' => []]);
     HTML::component('buttonIcon', 'components.html.button', ['title', 'icon' => null, 'attributes' => []]);
