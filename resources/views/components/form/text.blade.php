0552f33c67777289568950d47a6468b5d1b8a512
fauzigalih <fauzigalihas@gmail.com>
2020-08-09 22:34:53 +0700

    try display invalid data

--- resources/views/components/form/text.blade.php
+++ resources/views/components/form/text.blade.php
@@ -3,10 +3,15 @@
     {{ Form::text($name, $value, 
         array_merge(
             [
-                'class' => 'form-control', 
+                "class" => "form-control" . "@error('invoice') is-invalid @enderror", 
                 'placeholder' => ucwords(str_replace('_', ' ', $name)),
                 'id' => $name,
             ], 
             $attributes,
         )) }}
+    @error($name)
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+    @enderror
 </div>
