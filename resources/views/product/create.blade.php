f1f8d53b2401d8d7ebcc221a1eb34bc0ca06c286
fauzigalih <fauzigalihas@gmail.com>
2020-08-10 23:27:48 +0700

    update value form unit and display flash error

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -9,14 +9,24 @@
         <div class="bgc-white p-20 bd">
             <h4 class="c-grey-900">{{ $namePage }}</h4>
             <div class="mT-30">
-                {!! Form::open(['url' => 'product']) !!}
+                
+                {!! Form::open(['url' => 'product', 'autocomplete' => false]) !!}
+                @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
                     <div class="form-row">
                         {!! Form::textGroup('invoice', null, ['autofocus' => true]) !!}
                         {!! Form::textGroup('name_product') !!}
                         {!! Form::textGroup('type_product') !!}
                     </div>
                     <div class="form-row">
-                        {!! Form::selectGroup('unit', array('Pcs', 'Carton/Dus')) !!}
+                        {!! Form::selectGroup('unit', ['Pcs' => 'Pcs', 'Carton/Dus' => 'Carton/Dus']) !!}
                         {!! Form::numberGroup('price') !!}
                         {!! Form::numberGroup('stock_first') !!}
                     </div>
