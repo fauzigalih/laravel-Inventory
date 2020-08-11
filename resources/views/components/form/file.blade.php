864ba36a7214db4996338f3356a54b0618d44071
fauzigalih <fauzigalihas@gmail.com>
2020-08-11 21:32:50 +0700

    custom class invalid display

--- resources/views/components/form/file.blade.php
+++ resources/views/components/form/file.blade.php
@@ -1,12 +1,20 @@
+@php
+    $invalid = ($errors->get($name) ? 'form-control-file is-invalid' : 'form-control-file');
+@endphp
 <div class="form-group col-md-4">
     {{ Form::label($name, null, ['class' => 'control-label']) }}
     {{ Form::file($name,
         array_merge(
             [
-                'class' => 'form-control-file', 
+                'class' => $invalid, 
                 'placeholder' => ucwords(str_replace('_', ' ', $name)),
                 'id' => $name,
             ], 
             $attributes,
         )) }}
+    @error ($name)
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+    @enderror
 </div>
