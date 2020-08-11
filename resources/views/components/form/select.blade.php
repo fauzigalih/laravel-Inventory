5118a948e893b02e15d550bee7e3b78196e354dc
fauzigalih <fauzigalihas@gmail.com>
2020-08-11 21:33:32 +0700

    custom class invalid display

--- resources/views/components/form/select.blade.php
+++ resources/views/components/form/select.blade.php
@@ -1,8 +1,16 @@
+@php
+    $invalid = ($errors->get($name) ? 'form-control is-invalid' : 'form-control');
+@endphp
 <div class="form-group col-md-4">
     {{ Form::label($name, null, ['class' => 'control-label']) }}
     {{ Form::select($name, $value ?? ['One', 'Two', 'Three'], null, array_merge(
         [
-            'class' => 'form-control', 
+            'class' => $invalid, 
             'placeholder' => 'Select ' . ucwords(str_replace('_', ' ', $name)),
         ], $attributes)) }}
+    @error ($name)
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+    @enderror
 </div>
