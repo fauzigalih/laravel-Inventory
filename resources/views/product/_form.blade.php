545d5fa8fde05c89a307ef5cd9d5b5b93b333b99
fauzigalih <fauzigalihas@gmail.com>
2020-08-13 22:52:31 +0700

    resolve and refactor form

--- resources/views/product/_form.blade.php
+++ resources/views/product/_form.blade.php
@@ -1,30 +1,38 @@
 @php
-    $action = Route::currentRouteName();
+    use App\Product;
+    use App\User;
+
     $activeInvoice = Route::currentRouteName() === 'product.create' ? $model->invoiceData() : $model->invoice;
-    $autofocus = Route::currentRouteName() === 'product.create' ? true : false;
-    $isReadonly = Route::currentRouteName() === 'product.show' ? true : false;
+    $actionCreate = Route::currentRouteName() === 'product.create';
+    $isReadonly = Route::currentRouteName() === 'product.show';
+    $actionUpdate = Route::currentRouteName() === 'product.edit';
+    $urlForm = Route::currentRouteName() === 'product.edit' ? 'product/'.$model->id : 'product';
 @endphp
-{{ $action }}
-{!! Form::open(['url' => 'product', 'autocomplete' => 'off']) !!}
+{!! Form::open(['url' => $urlForm, 'autocomplete' => 'off']) !!}
+    @if ($actionUpdate)
+        @method('PUT')
+    @endif
     <div class="form-row">
         {!! Form::textGroup('invoice', $activeInvoice, ['readonly' => true]) !!}
-        {!! Form::textGroup('name_product', $model->name_product, ['autofocus' => $autofocus, 'readonly' => $isReadonly]) !!}
-        {!! Form::textGroup('type_product') !!}
+        {!! Form::textGroup('name_product', $model->name_product, ['autofocus' => $actionCreate, 'readonly' => $isReadonly]) !!}
+        {!! Form::textGroup('type_product', $model->type_product, ['readonly' => $isReadonly]) !!}
     </div>
     <div class="form-row">
-        {!! Form::selectGroup('unit', ['Pcs' => 'Pcs', 'Carton/Dus' => 'Carton/Dus']) !!}
-        {!! Form::numberGroup('price') !!}
-        {!! Form::numberGroup('stock_first') !!}
+        {!! Form::selectGroup('unit', Product::$unitCategories, $model->unit) !!}
+        {!! Form::numberGroup('price', $model->price, ['readonly' => $isReadonly]) !!}
+        {!! Form::numberGroup('stock_first', $model->stock_first, ['readonly' => $isReadonly]) !!}
     </div>
     <div class="form-row">
-        {!! Form::numberGroup('stock_in') !!}
-        {!! Form::numberGroup('stock_out') !!}
-        {!! Form::numberGroup('stock_final') !!}
+        {!! Form::numberGroup('stock_in', $model->stock_in, ['readonly' => $isReadonly]) !!}
+        {!! Form::numberGroup('stock_out', $model->stock_out, ['readonly' => $isReadonly]) !!}
+        {!! Form::numberGroup('stock_final', $model->stock_final, ['readonly' => $isReadonly]) !!}
     </div>
     <div class="form-row">
-        {!! Form::fileGroup('image_product') !!}
-        {!! Form::selectGroup('active', [1 => 'Active', 0 => 'Not Active']) !!}
+        {!! Form::fileGroup('image_product', null, ['readonly' => $isReadonly]) !!}
+        {!! Form::selectGroup('active', User::$activeCategories, $model->active, ['readonly' => $isReadonly]) !!}
     </div>
-    {!! HTML::buttonIcon('Save', 'fas fa-plus') !!}
+    @if (!$isReadonly)
+        {!! HTML::buttonIcon('Save', 'fas fa-plus') !!}
+    @endif
     {!! HTML::linkIcon(url()->previous(), 'Back', 'fas fa-arrow-left', ['class' => 'btn btn-danger']) !!}
 {!! Form::close() !!}
