fcb4b8d6f0ae5fb1439ffeced934c4519aec9016
fauzigalih <fauzigalihas@gmail.com>
2020-08-13 22:51:15 +0700

    resolve form component and add html component for action button

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -3,10 +3,12 @@
     Form::component('emailGroup', 'components.form.email', ['name', 'value' => null, 'attributes' => []]);
     Form::component('passwordGroup', 'components.form.password', ['name', 'value' => null, 'attributes' => []]);
     Form::component('numberGroup', 'components.form.number', ['name', 'value' => null, 'attributes' => []]);
-    Form::component('fileGroup', 'components.form.file', ['name', 'attributes' => []]);
-    Form::component('selectGroup', 'components.form.select', ['name', 'value' => null, 'attributes' => []]);
+    Form::component('fileGroup', 'components.form.file', ['name', 'value', 'attributes' => []]);
+    Form::component('selectGroup', 'components.form.select', ['name', 'list' => null, 'value' => null, 'attributes' => []]);
     Form::component('selectTypeGroup', 'components.form.selectType', ['name', 'value' => null, 'attributes' => []]);
 
     HTML::component('linkIcon', 'components.html.link', ['url', 'title', 'icon' => null, 'attributes' => []]);
     HTML::component('buttonIcon', 'components.html.button', ['title', 'icon' => null, 'attributes' => []]);
+    
+    HTML::component('actionButton', 'components.html.action', ['route', 'model']);
 @endphp
