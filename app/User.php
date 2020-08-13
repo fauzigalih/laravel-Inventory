61319159ba1e39c4556b513abe072569fe6b94bc
fauzigalih <fauzigalihas@gmail.com>
2020-08-13 22:48:23 +0700

    add active categories array

--- app/User.php
+++ app/User.php
@@ -36,4 +36,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public static $activeCategories = [
+        1 => 'Active',
+        0 => 'Not Active'
+    ];
 }
