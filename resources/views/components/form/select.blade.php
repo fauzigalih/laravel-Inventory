7d08ef0cf4f3cd50711a17db0775b563efb4d343
fauzigalih <fauzigalihas@gmail.com>
2020-08-13 22:49:52 +0700

    refactor list and value

--- resources/views/components/form/select.blade.php
+++ resources/views/components/form/select.blade.php
@@ -3,7 +3,7 @@
 @endphp
 <div class="form-group col-md-4">
     {{ Form::label($name, null, ['class' => 'control-label']) }}
-    {{ Form::select($name, $value ?? ['One', 'Two', 'Three'], null, array_merge(
+    {{ Form::select($name, $list ?? ['One', 'Two', 'Three'], $value, array_merge(
         [
             'class' => $invalid, 
             'placeholder' => 'Select ' . ucwords(str_replace('_', ' ', $name)),
