16c91cc8657ae562cd6106c3baae7b4a81076db8
fauzigalih <fauzigalihas@gmail.com>
2020-08-14 22:30:06 +0700

    remove value

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -3,7 +3,7 @@
     Form::component('emailGroup', 'components.form.email', ['name', 'value' => null, 'attributes' => []]);
     Form::component('passwordGroup', 'components.form.password', ['name', 'value' => null, 'attributes' => []]);
     Form::component('numberGroup', 'components.form.number', ['name', 'value' => null, 'attributes' => []]);
-    Form::component('fileGroup', 'components.form.file', ['name', 'value', 'attributes' => []]);
+    Form::component('fileGroup', 'components.form.file', ['name', 'attributes' => []]);
     Form::component('selectGroup', 'components.form.select', ['name', 'list' => null, 'value' => null, 'attributes' => []]);
     Form::component('selectTypeGroup', 'components.form.selectType', ['name', 'value' => null, 'attributes' => []]);
 
