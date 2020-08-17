6c9ddc24deadd2ecb310bb3921d06cf1afcfe17d
fauzigalih <fauzigalihas@gmail.com>
2020-08-17 21:26:03 +0700

    refactoring

--- resources/views/components/html/action.blade.php
+++ resources/views/components/html/action.blade.php
@@ -1,14 +1,11 @@
 <div class="btn-group" role="group" aria-label="Basic example">
-    {!! Form::open(['url' => $route.'/'.$model->id]) !!}
-        @method('GET')
+    {!! Form::open(['route' => [$route.'.show', $model->id], 'method' => 'GET']) !!}
         {!! HTML::buttonIcon(null, 'far fa-eye', ['class' => 'btn btn-link']) !!}
     {!! Form::close() !!}
-    {!! Form::open(['url' => $route.'/'.$model->id.'/edit']) !!}
-        @method('GET')
+    {!! Form::open(['route' => [$route.'.edit', $model->id], 'method' => 'GET']) !!}
         {!! HTML::buttonIcon(null, 'far fa-edit', ['class' => 'btn btn-link']) !!}
     {!! Form::close() !!}
-    {!! Form::open(['url' => $route.'/'.$model->id]) !!}
-        @method('DELETE')
+    {!! Form::open(['route' => [$route.'.destroy', $model->id], 'method' => 'DELETE']) !!}
         {!! HTML::buttonIcon(null, 'far fa-trash-alt', ['class' => 'btn btn-link']) !!}
     {!! Form::close() !!}
 </div>
