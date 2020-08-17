390d15daf1c28e1d05ed3aa81e8c7bab8a069f6b
fauzigalih <fauzigalihas@gmail.com>
2020-08-17 21:25:42 +0700

    fix model

--- app/ProductIn.php
+++ app/ProductIn.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductIn extends Model
 {
-    //
+    public $namePage = 'Product In';
+
+    protected $fillable = [
+        'invoice',
+        'user_id',
+        'product_id',
+        'qty_in'
+    ];
+
+    public function invoiceData() {
+        $query = self::max('invoice');
+        $noInvoice = (int) substr($query, 3, 3);
+        $noInvoice++;
+        $charInvoice = "PIN";
+        $newInvoice = $charInvoice . sprintf("%03s", $noInvoice);
+
+        return $newInvoice;
+    }
 }
