17f58d2eed280710923c17c910c52e67d1c4b6bd
fauzigalih <fauzigalihas@gmail.com>
2020-08-17 21:26:51 +0700

    refactor

--- resources/views/product-in/index.blade.php
+++ resources/views/product-in/index.blade.php
@@ -1,15 +1,15 @@
 @include('components.main')
 @extends('layout.main')
 
-@section('title', $namePage)
+@section('title', $model->namePage)
 @section('content')
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-12">
             <div class="bgc-white bd bdrs-3 p-20 mB-20">
                 @include('layout.alert')
-                <h4 class="c-grey-900 mB-20">{{ $namePage }}</h4> {{ $a }}
-                {!! HTML::linkIcon('/product/create', 'Create', 'fas fa-plus', ['class' => 'btn btn-primary']) !!}
+                <h4 class="c-grey-900 mB-20">{{ $model->namePage }}</h4>
+                {!! HTML::linkIcon(route('product-in.create'), 'Create', 'fas fa-plus', ['class' => 'btn btn-primary']) !!}
                 <table id="dataTable" class="table table-striped table-bordered" cellspacing="0" width="100%">
                     <thead>
                         <tr>
@@ -24,16 +24,16 @@
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach ($model as $data)
+                        @foreach ($model->all() as $data)
                             <tr>
                                 <td>{{ $loop->iteration }}</td>
-                                <td>{{ $data->invoice ?? '' }}</td>
-                                <td>{{ $data->user_id ??  ''}}</td>
-                                <td>{{ $data->product_id ?? '' }}</td>
+                                <td>{{ $data->invoice }}</td>
+                                <td>{{ $data->user_id }}</td>
+                                <td>{{ $data->product_id }}</td>
                                 <td>{{ $data->getProduct->price ?? '' }}</td>
-                                <td>{{ $data->qty_in ?? '' }}</td>
+                                <td>{{ $data->qty_in }}</td>
                                 <td>{{ $data->getProduct->image_product ?? '' }}</td>
-                                <td>{!! HTML::actionButton('product_in', $data) !!}</td>
+                                <td>{!! HTML::actionButton('product-in', $data) !!}</td>
                             </tr>
                         @endforeach
                     </tbody>
