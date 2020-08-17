82c4676012b6f3b0c2bdfbf2ce07c53ff6077d73
fauzigalih <fauzigalihas@gmail.com>
2020-08-17 21:27:18 +0700

    create page view

---
+++ resources/views/product-in/view.blade.php
@@ -0,0 +1,17 @@
+@include('components.main')
+@extends('layout.main')
+
+@section('title', 'View ' . $model->namePage)
+@section('content')
+<div class="row gap-20 masonry pos-r">
+    <div class="masonry-sizer col-md-6"></div>
+    <div class="masonry-item col-md-12">
+        <div class="bgc-white p-20 bd">
+            <h4 class="c-grey-900">{{ 'View ' . $model->namePage }}</h4>
+            <div class="mT-30">
+                @include('product-in._form')
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
