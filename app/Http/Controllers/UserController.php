a319c70679a98bbccc0ffc01baad2136eaf4408d
fauzigalih <fauzigalihas@gmail.com>
2020-08-20 21:49:23 +0700

    add func home

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use App\Product;
+use App\ProductIn;
+use App\ProductOut;
+use App\Transaction;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -81,5 +90,13 @@ class UserController extends Controller
     public function destroy(User $user)
     {
         //
+    }
+
+    public function home() {
+        $product = Product::count();
+        $productIn = ProductIn::count();
+        $productOut = ProductOut::count();
+        $transaction = Transaction::count();
+        return view('user.home', compact('product', 'productIn', 'productOut', 'transaction'));
     }
 }
