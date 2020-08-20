961afcc793c2610bbfc4c9077b9f4fad976e93f6
fauzigalih <fauzigalihas@gmail.com>
2020-08-20 21:50:32 +0700

    soft delete, default value, fix invoice, add updateStock, add list product

--- app/Product.php
+++ app/Product.php
@@ -3,10 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
+    use SoftDeletes;
+
     public $namePage = 'Product';
+
+    protected $attributes = [
+        'stock_in' => 0,
+        'stock_out' => 0
+    ];
 
     protected $fillable = [
         'invoice',
@@ -29,12 +37,32 @@ class Product extends Model
     ];
 
     public function invoiceData() {
-        $query = self::max('invoice');
+        $query = self::withTrashed()->max('invoice');
         $noInvoice = (int) substr($query, 3, 3);
         $noInvoice++;
         $charInvoice = "INV";
         $newInvoice = $charInvoice . sprintf("%03s", $noInvoice);
 
         return $newInvoice;
+    }
+
+    public static function updateStock($type, $id, $stock, $actionUpdate = false, $newId = null)
+    {
+        $product = Product::findOrFail($id);
+        if ($type == 'stock_in') {
+            $final = $product->stock_first + $product->stock_in - $product->stock_out + $stock;
+            self::where('id', $id)->update([$type => $stock, 'stock_final' => $final]);
+            return true;
+        }else if ($type == 'stock_out') {
+            $final = $product->stock_first + $product->stock_in - $product->stock_out - $stock;
+            return self::where('id', $id)->update([$type => $stock, 'stock_final' => $final]);
+        }
+        
+        return false;
+    }
+
+    public static function listProduct()
+    {
+        return self::pluck('name_product', 'id');
     }
 }
