641a8d13b4b65672bed88f9a01faf3702b5ae44f
fauzigalih <fauzigalihas@gmail.com>
2020-08-21 23:00:34 +0700

    Update alert.blade.php

--- resources/views/layout/alert.blade.php
+++ resources/views/layout/alert.blade.php
@@ -1,6 +1,6 @@
-@if(session('success'))
+@if(session('success') || session('status'))
     <div class="alert alert-success alert-dismissible fade show" role="alert">
-        {{ session('success') }}
+        {{ session('success') ?? session('status') }}
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
