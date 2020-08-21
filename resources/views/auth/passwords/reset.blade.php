86078def4a46d30740b39077458b89b27bba5943
fauzigalih <fauzigalihas@gmail.com>
2020-08-21 23:00:25 +0700

    Create reset.blade.php

---
+++ resources/views/auth/passwords/reset.blade.php
@@ -0,0 +1,47 @@
+@php
+    $invalidEmail = $errors->get('email') ? 'form-control is-invalid' : 'form-control';
+    $invalidPassword = $errors->get('password') ? 'form-control is-invalid' : 'form-control';
+@endphp
+@extends('layout.main_auth')
+@section('title', 'Reset Password')
+@section('content')
+<h4 class="fw-300 c-grey-900 mB-40">{{ __('Reset Password') }}</h4>
+{!! Form::open(['route' => 'password.update']) !!}
+    <input type="hidden" name="token" value="{{ $token ?? '' }}">
+    <div class="form-group">
+        {!! Form::label('email', null, ['class' => 'text-normal text-dark']); !!}
+        {!! Form::email('email', old('email'), ['class' => $invalidEmail, 'placeholder' => 'Email']) !!}
+        @error ('email')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+        @enderror
+    </div>
+    <div class="form-group">
+        {!! Form::label('password', null, ['class' => 'text-normal text-dark']); !!}
+        {!! Form::password('password', ['class' => $invalidPassword, 'placeholder' => 'Password']) !!}
+        @error ('password')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+        @enderror
+    </div>
+    <div class="form-group">
+        {!! Form::label('confirm_password', null, ['class' => 'text-normal text-dark']); !!}
+        {!! Form::password('password_confirmation', ['class' => 'form-control', 'placeholder' => 'Confirm Password']) !!}
+        @error ('password_confirmation')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+        @enderror
+    </div>
+    <div class="form-group">
+        {!! Form::submit('Reset Password', ['class' => 'btn btn-primary']); !!}
+    </div>
+    @if (Route::has('login'))
+        <a class="btn btn-link" href="{{ route('login') }}">
+            {{ __('Already have an account?') }}
+        </a>
+    @endif
+{!! Form::close() !!}
+@endsection
