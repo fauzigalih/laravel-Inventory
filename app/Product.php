18965ddb9636b4bbe72e36979c20aca42eb57f2c
fauzigalih <fauzigalihas@gmail.com>
2020-08-22 21:42:52 +0700

    refactor model update stock

--- app/Product.php
+++ app/Product.php
@@ -4,6 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route as FacadesRoute;
 
 class Product extends Model
 {
@@ -36,6 +38,20 @@ class Product extends Model
         'Dus' => 'Dus',
     ];
 
+    public static function validateData(Request $request)
+    {
+        $update = FacadesRoute::currentRouteName() === 'product.update';
+        $request->validate(array_merge([
+            'invoice' => 'required|size:6|unique:products,invoice|string',
+            'name_product' => 'required|string',
+            'type_product' => 'required|string',
+            'unit' => 'required|string',
+            'price' => 'required|integer',
+            'stock_first' => 'required|integer',
+            'active' => 'required|integer',
+        ], $update ? ['invoice' => 'required|size:6|string'] : ['image_product' => 'required']));
+    }
+
     public function invoiceData() {
         $query = self::withTrashed()->max('invoice');
         $noInvoice = (int) substr($query, 3, 3);
@@ -46,16 +62,32 @@ class Product extends Model
         return $newInvoice;
     }
 
-    public static function updateStock($type, $id, $stock, $actionUpdate = false, $newId = null)
+    public static function updateStock($id, $stock, $newId = null, $newStock = null)
     {
-        $product = Product::findOrFail($id);
-        if ($type == 'stock_in') {
-            $final = $product->stock_first + $product->stock_in - $product->stock_out + $stock;
-            self::where('id', $id)->update([$type => $stock, 'stock_final' => $final]);
+        $storeProductIn = FacadesRoute::currentRouteName() === 'product-in.store';
+        $updateProductIn = FacadesRoute::currentRouteName() === 'product-in.update';
+        $storeProductOut = FacadesRoute::currentRouteName() === 'product-out.store';
+        $updateProductOut = FacadesRoute::currentRouteName() === 'product-out.update';
+        $product = self::findOrFail($id);
+        $final = $product->stock_first + $product->stock_in - $product->stock_out;
+        if ($storeProductIn || $storeProductOut) {
+            if ($storeProductIn) {
+                self::where('id', $id)->update(['stock_in' => $product->stock_in + $stock, 'stock_final' => $final += $stock]);
+            }else if($storeProductOut){
+                self::where('id', $id)->update(['stock_out' => $product->stock_out + $stock, 'stock_final' => $final -= $stock]);
+            }
             return true;
-        }else if ($type == 'stock_out') {
-            $final = $product->stock_first + $product->stock_in - $product->stock_out - $stock;
-            return self::where('id', $id)->update([$type => $stock, 'stock_final' => $final]);
+        }else if ($updateProductIn || $updateProductOut) {
+            $productNew = self::findOrFail($newId);
+            $updateStock = $newStock - $stock;
+            if ($updateProductIn) {
+                if($id !== $newId) self::where('id', $id)->update(['stock_in' => $product->stock_in - $stock, 'stock_final' => $final -= $stock]);
+                self::where('id', $newId)->update(['stock_in' => $productNew->stock_in += ($id == $newId) ? $updateStock : $newStock, 'stock_final' => $final += ($id == $newId) ? $updateStock : $newStock]);
+            }else if($updateProductOut){
+                if($id !== $newId) self::where('id', $id)->update(['stock_out' => $product->stock_out - $stock, 'stock_final' => $final += $stock]);
+                self::where('id', $newId)->update(['stock_out' => $productNew->stock_in += ($id == $newId) ? $updateStock : $newStock, 'stock_final' => $final -= ($id == $newId) ? $updateStock : $newStock]);
+            }
+            return true;
         }
         
         return false;
