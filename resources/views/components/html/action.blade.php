895f21d637228856b1211854797709b4cae6ec0e
fauzigalih <fauzigalihas@gmail.com>
2020-08-24 08:51:04 +0700

    resolve button icon not registered

--- resources/views/components/html/action.blade.php
+++ resources/views/components/html/action.blade.php
@@ -1,14 +1,14 @@
 <div class="btn-group" role="group" aria-label="Basic example">
     {!! Form::open(['route' => [$route.'.show', $model->id]]) !!}
         @method('GET')
-        {!! HTML::buttonIcon(null, 'far fa-eye', ['class' => 'btn btn-link']) !!}
+        {!! HTML::buttonIconCustom(null, 'far fa-eye', ['class' => 'btn btn-link']) !!}
     {!! Form::close() !!}
     {!! Form::open(['route' => [$route.'.edit', $model->id]]) !!}
         @method('GET')
-        {!! HTML::buttonIcon(null, 'far fa-edit', ['class' => 'btn btn-link']) !!}
+        {!! HTML::buttonIconCustom(null, 'far fa-edit', ['class' => 'btn btn-link']) !!}
     {!! Form::close() !!}
     {!! Form::open(['route' => [$route.'.destroy', $model->id]]) !!}
         @method('DELETE')
-        {!! HTML::buttonIcon(null, 'far fa-trash-alt', ['class' => 'btn btn-link']) !!}
+        {!! HTML::buttonIconCustom(null, 'far fa-trash-alt', ['class' => 'btn btn-link']) !!}
     {!! Form::close() !!}
 </div>
